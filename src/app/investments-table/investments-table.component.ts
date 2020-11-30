e6876de8ad93822451ae523334af158122eed50c
Raphael <raphael.m.arantes@accenture.com>
2020-11-30 20:23:54 -0300

    add investment table

---
+++ src/app/investments-table/investments-table.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { Investment } from '../models/product';
+import { ProductsService } from '../services/products.service';
+import { } from '../common/app-error';
+import * as moment from 'moment';
+import { ShareDataService } from '../services/share-data.service.spec';
+import { HelperService } from '../services/helper.service';
+import { Router } from '@angular/router';
+
+const EMPTY = '';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './investments-table.component.html',
+  styleUrls: ['./investments-table.component.less']
+})
+
+export class InvestmentsTableComponent implements OnInit {
+  products: Investment[] = [];
+  product: Investment;
+  tableMessageError: string;
+  showTable: boolean;
+  noDataMessage: string;
+
+  constructor(private service: ProductsService,
+    private shareData: ShareDataService,
+    private helperService: HelperService,
+    private router: Router
+  ) {
+    this.product = new Investment();
+    this.showTable = true;
+    this.tableMessageError = "";
+    this.noDataMessage = 'There are no investments for while';
+  }
+
+
+  ngOnInit() {
+    this.listInvestments();
+  }
+
+  listInvestments() {
+    console.log('listou')
+    this.showTable = true;
+    this.service.getProducts().subscribe(
+      (products) => {
+        this.tableMessageError = '';
+        products['investments'].map(p => {
+          this.product._id = p._id;
+          this.product.type = p.type;
+          this.product.value = p.value;
+          this.product.date = this.helperService.formatDateGet(p.date)
+          this.products.push({ ...this.product });
+        });
+      },
+      (error) => {
+        this.tableMessageError = 'List investments failed';
+      }
+    );
+  }
+
+  itemFormSelected(p) {
+    this.transferItemForm(p);
+    this.router.navigate(['/products', 'update'])
+  }
+
+  transferItemForm(p) {
+    this.product._id = p._id;
+    this.product.type = p.type;
+    this.product.value = p.value;
+    this.shareData.sendData({ ...this.product });
+  }
+
+  onDelete(productId) {
+    if (confirm('Are you sure?')) {
+      this.service.deleteProduct(productId).subscribe(
+        () => {
+          this.products = this.products.filter(
+            product => product._id !== productId
+          );
+        },
+        (error) => {
+          this.tableMessageError = 'Delete investment failed';
+        }
+      );
+    }
+  }
+}
