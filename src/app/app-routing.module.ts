9219dc852173b00c42994b8d5e910a6bd4c0c9d0
Raphael <raphael.m.arantes@accenture.com>
2020-11-30 19:51:42 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InvestmentFormComponent } from './investment-form/investment-form.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { InvestmentsTableComponent } from './investments-table/investments-table.component';
+
+
+const appRoutes: Routes = [
+  { path: '' || 'products', component: InvestmentsTableComponent },
+  { path: 'products/new', component: InvestmentFormComponent },
+  { path: 'products/:update', component: InvestmentFormComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { InvestmentsTableComponent } from './investments-table/investments-table.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { InvestmentFormComponent } from './investment-form/investment-form.component';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { TextMaskModule } from 'angular2-text-mask';
+import { ProductsService } from './services/products.service';
+import { ShareDataService } from './services/share-data.service.spec';
+import { HelperService } from './services/helper.service';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppNavComponent,
+    InvestmentsTableComponent,
+    NotFoundComponent,
+    InvestmentFormComponent,
+    ProductDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    CurrencyMaskModule,
+    TextMaskModule
+  ],
+  providers: [ProductsService, ShareDataService, HelperService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
